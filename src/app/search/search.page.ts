ad16b783a44daf3bada2dbaf3696d75d135bfa35
MisakiShimazaki <msk.toybox@gmail.com>
2022-01-09 14:13:58 +0900

    1 file added, 1 deleted

---
+++ src/app/search/search.page.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.page.html',
+  styleUrls: ['./search.page.css'],
+})
+export class SearchPage {
+  constructor() {}
+}
